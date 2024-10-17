bd375705298982f7827ab9c6b039f9c93ebc062e
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2024-10-17 23:30:11 +0300

    added features pushed my milli

--- resources/views/userprofile.blade.php
+++ resources/views/userprofile.blade.php
@@ -12,7 +12,26 @@
                     <i class='bx bx-search'></i>
                     <input type="text" placeholder="search">
                 </div>
-                <!--user image-->
+            </div>
+        </div>
+        <!--user image-->
+        <div>
+            <br>
+            <img src="#" alt="user image">
+            <br>
+            <h1>user name </h1>
+            <div>
+                <form action="#" method="POST">
+                    @csrf
+                    <label for="full-name">Full name:</label>
+                    <input type="text" id="full-name" name="full_name" required>
+
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+
+                    <label for="phonenumber">Phone Number:</label>
+                    <input type="tel" id="phoneNumber" name="phoneNumber" required>
+                </form>
             </div>
         </div>
     </div>

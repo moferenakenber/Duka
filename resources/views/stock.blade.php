8b3d3a58898d14454d32f11abdfe767e85bffffb
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2024-10-21 05:10:12 +0300

    added dashboardtest.blade.php, also run npm run build to build the projects vite resources,

---
+++ resources/views/stock.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.user_type.auth')
+
+@section('content')
+
+    <div class="main--content">
+        <div class="header--wrapper">
+            <div class="header--title">
+                <h2>Stock</h2>
+            </div>
+            <div class="user--info">
+                <div class="search--box">
+                    <i class='bx bx-search'></i>
+                    <input type="text" placeholder="search">
+                </div>
+                <!--user image-->
+            </div>
+            <a href="additempage" class="additem">+</a>
+        </div>
+    </div>
+
+@endsection

3350699839d64655dbc5046967d5ad924cc9bfb2
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2024-11-12 04:13:02 +0300

    horizontally center

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,10 +8,13 @@
                 <h2>Dashboard</h2>
             </div>
         </div>
-        <video width="640" height="360" controls>
-            <source src="{{ asset('videos/U2-BeautifulDay.mp4') }}" type="video/mp4">
-            Your browser does not support the video tag.
-        </video>
+        <div style="display: flex; justify-content: center;">
+            <video width="640" height="360" controls>
+                <source src="{{ asset('videos/U2-BeautifulDay.mp4') }}" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+
 
     </div>
 

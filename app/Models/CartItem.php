5acf9eff1c62b24d3173715e960451cf8656380e
yonathan hunetaw <160973598+wibdar@users.noreply.github.com>
2024-11-12 03:28:14 +0300

    aha

---
+++ app/Models/CartItem.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CartItems extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'cart_id',
+        'item_id',
+        'quantity',
+        'price'];
+
+    public function cart(): BelongsTo
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function item(): BelongsTo
+    {
+        return $this->belongsTo(Item::class);
+    }
+}
